173f4533caa22890fe4432aab350047543130a1e
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-07-22 20:00:23 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -93,9 +93,8 @@ typedef struct passinfo
 } info_t;
 
 #define INFO_INIT \
-
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL,
-0, 0, 0}
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 /**
  * struct builtin - contains a string and a function
