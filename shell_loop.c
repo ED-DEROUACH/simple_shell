b9c86a1801300f362e708ff7efd64b91809597c1
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-07-23 16:14:53 +0100

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -12,7 +12,8 @@ int hsh(info_t *info, char **av)
 	ssize_t r = 0;
 	int builtin_ret = 0;
 
-	do {
+	while (r != -1 && builtin_ret != -2)
+	{
 		clear_info(info);
 		if (interactive(info))
 			_puts("$ ");
@@ -28,8 +29,7 @@ int hsh(info_t *info, char **av)
 		else if (interactive(info))
 			_putchar('\n');
 		free_info(info, 0);
-	} while (r != -1 && builtin_ret != -2);
-
+	}
 	write_history(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
@@ -54,8 +54,7 @@ int hsh(info_t *info, char **av)
  */
 int find_builtin(info_t *info)
 {
-	int i = 0;
-	int built_in_ret = -1;
+	int i, built_in_ret = -1;
 	builtin_table builtintbl[] = {
 		{"exit", _myexit},
 		{"env", _myenv},
@@ -68,17 +67,13 @@ int find_builtin(info_t *info)
 		{NULL, NULL}
 	};
 
-	while (builtintbl[i].type != NULL)
-	{
+	for (i = 0; builtintbl[i].type; i++)
 		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
 		{
 			info->line_count++;
 			built_in_ret = builtintbl[i].func(info);
 			break;
 		}
-		i++;
-	}
-
 	return (built_in_ret);
 }
 
@@ -91,7 +86,7 @@ int find_builtin(info_t *info)
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
-	int i = 0, k = 0;
+	int i, k;
 
 	info->path = info->argv[0];
 	if (info->linecount_flag == 1)
@@ -99,13 +94,9 @@ void find_cmd(info_t *info)
 		info->line_count++;
 		info->linecount_flag = 0;
 	}
-
-	do {
+	for (i = 0, k = 0; info->arg[i]; i++)
 		if (!is_delim(info->arg[i], " \t\n"))
 			k++;
-		i++;
-	} while (info->arg[i]);
-
 	if (!k)
 		return;
 
@@ -117,9 +108,9 @@ void find_cmd(info_t *info)
 	}
 	else
 	{
-		if ((interactive(info) || _getenv(info, "PATH=") || info->
-					argv[0][0] == '/') && find_cmd(info, info->argv[0]))
-		fork_cmd(info);
+		if ((interactive(info) || _getenv(info, "PATH=")
+			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
 		else if (*(info->arg) != '\n')
 		{
 			info->status = 127;
@@ -141,34 +132,29 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
+		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
-
-	switch (child_pid)
+	if (child_pid == 0)
 	{
-		case (-1):
-			perror("Error:");
-			break;
-
-		case (0):
-			if (execve(info->path, info->argv, get_environ(info)) == -1)
-			{
-				free_info(info, 1);
-				if (errno == EACCES)
-					exit(126);
-				exit(1);
-			}
-			break;
-
-		default:
-			wait(&(info->status));
-			if (WIFEXITED(info->status))
-			{
-				info->status = WEXITSTATUS(info->status);
-				if (info->status == 126)
-					print_error(info, "Permission denied\n");
-			}
-			break;
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
 	}
 }
