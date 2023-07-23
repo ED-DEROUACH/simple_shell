0651d533840bb7e95b91ceb855cf1dece8e19c92
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-07-23 01:58:21 +0100

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -57,7 +57,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	*p = malloc(new_size);
+	p = malloc(new_size);
 
 	if (!p)
 
