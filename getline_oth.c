7d5bafd999b19ebae4c140ccd4fda414edb86053
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-07-26 11:45:36 +0100

    Update getline_oth.c

--- getline_oth.c
+++ getline_oth.c
@@ -49,9 +49,8 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  */
 ssize_t get_input(info_t *info)
 {
-	static char *buf = NULL;
-	static size_t len = 0;
-	static int i = 0;
+	static char *buf;
+	static size_t i, j, len;
 	ssize_t r = 0;
 	char **buf_p = &(info->arg), *p;
 
@@ -59,26 +58,30 @@ ssize_t get_input(info_t *info)
 	r = input_buf(info, &buf, &len);
 	if (r == -1)
 		return (-1);
-
 	if (len)
 	{
+		j = i;
 		p = buf + i;
-		check_chain(info, buf, &i, 0, len);
-
+		check_chain(info, buf, &j, i, len);
+		while (j < len)
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+		i = j + 1;
 		if (i >= len)
 		{
 			i = len = 0;
 			info->cmd_buf_type = CMD_NORM;
-			info->cmd_buf = NULL;
 		}
-
 		*buf_p = p;
 		return (_strlen(p));
 	}
-
 	*buf_p = buf;
 	return (r);
 }
+
 
 /**
  * read_buf - read the buffer
