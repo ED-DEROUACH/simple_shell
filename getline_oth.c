7d6f6aca68dbd546d8c16902bb52b2a93cb58e90
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-07-26 11:07:30 +0100

    Update getline_oth.c

--- getline_oth.c
+++ getline_oth.c
@@ -49,37 +49,31 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  */
 ssize_t get_input(info_t *info)
 {
-	static char *buf;
-	static size_t i, j, len;
-	ssize_t r = 0;
-	char **buf_p = &(info->arg), *p;
+ssize_t r;
+char *prompt = "$ ";
+size_t bufsize = READ_BUF_SIZE;
+size_t cmd_len;
 
-	_putchar(BUF_FLUSH);
-	r = input_buf(info, &buf, &len);
-	if (r == -1)
-		return (-1);
-	if (len)
-	{
-		j = i;
-		p = buf + i;
-		check_chain(info, buf, &j, i, len);
-		while (j < len)
-		{
-			if (is_chain(info, buf, &j))
-				break;
-			j++;
-		}
-		i = j + 1;
-		if (i >= len)
-		{
-			i = len = 0;
-			info->cmd_buf_type = CMD_NORM;
-		}
-		*buf_p = p;
-		return (_strlen(p));
-	}
-	*buf_p = buf;
-	return (r);
+if (isatty(STDIN_FILENO) && !info->histcount)
+_puts(prompt);
+
+r = _getline(info, &(info->arg), &bufsize);
+
+if (r == -1)
+{
+if (isatty(STDIN_FILENO))
+_putchar('\n');
+return (-1);
+}
+
+if (r > 0 && !info->linecount_flag)
+build_history_list(info, info->arg, info->histcount);
+
+cmd_len = _strlen(info->arg);
+if (cmd_len > 0 && info->arg[cmd_len - 1] == '\n')
+info->arg[cmd_len - 1] = '\0';
+
+return (r);
 }
 
 /**
