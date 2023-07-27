cee952055607cf2dbfefebf49cefc584d1f0874b
othmane mekkaoui-alaoui <othamne.alaouimekkaoui@gmail.com>
2023-07-27 16:23:51 -0700

    update

--- shell_loop_e.c
+++ shell_loop_e.c
@@ -2,10 +2,10 @@
 
 /**
  * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
+ * @info: struct
+ * @av: argument vector
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 on success, if error -1, otherwise integer
  */
 int hsh(info_t *info, char **av)
 {
@@ -48,9 +48,6 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
